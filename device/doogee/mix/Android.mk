75f5a8ca27cd31d4a8b56178420654d8a5c7aaa1
abariusav <mail@abarov.ru>
2019-02-19 20:58:26 +0600

    files

--- device/doogee/mix/Android.mk
+++ device/doogee/mix/Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),doogeemix)
+ifeq ($(TARGET_DEVICE),mix)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 endif
--- device/doogee/mix/omni_mix.mk
+++ device/doogee/mix/omni_mix.mk
@@ -1,11 +1,11 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 PRODUCT_COPY_FILES += \
-    device/doogee/doogeemix/kernel:kernel
+    device/doogee/mix/kernel:kernel
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := doogeemix
-PRODUCT_NAME := omni_doogeemix
+PRODUCT_DEVICE := mix
+PRODUCT_NAME := omni_mix
 PRODUCT_BRAND := DOOGEE
 PRODUCT_MODEL := MIX
 PRODUCT_MANUFACTURER := DOOGEE
